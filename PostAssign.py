3beb971f377eb844f0f271bcca7eaf1d8116066d
Patcharapong10 <patcharapong20new@gmail.com>
2021-02-23 12:25:04 +0700

    comit

--- PostAssign.py
+++ PostAssign.py
@@ -13,9 +13,8 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 # ##############################begin plane
-#plane Class/Model
-class plane(db.Model):
-    id = db.Column(db.String(15), primary_key=True, unique=True)
+class planes(db.Model):
+    id = db.Column(db.String(13), primary_key=True, unique=True)
     nameplane = db.Column(db.String(50))
     genplane = db.Column(db.String(25))
     
@@ -24,73 +23,33 @@ class plane(db.Model):
         self.nameplane = nameplane
         self.genplane = genplane
 
-# Create a plane
-@app.route('/plane', methods=['POST'])
-def add_plane():
-    id = request.json['ID']
-    nameplane = request.json['nameplane']
-    genplane = request.json['genplane']
-
-    new_plane = plane(id, nameplane, genplane)
-
-    db.session.add(new_plane)
-    db.session.commit()
-
-    return plane_schema.jsonify(new_plane)
-
-# Update a plane
-@app.route('/plane/<id>', methods=['PUT'])
-def update_plane(id):
-    plane = plane.query.get(id)
-    
-    nameplane = request.json['nameplane']
-    genplane = request.json['genplane']
-
-    plane.nameplane = nameplane
-    plane.genplane = genplane
-
-    db.session.commit()
-
-    return plane_schema.jsonify(plane)
 
 
-# Delete plane
-@app.route('/plane/<id>', methods=['DELETE'])
-def delete_plane(id):
-    plane = plane.query.get(id)
-    db.session.delete(plane)
-    db.session.commit()
-    
-    return plane_schema.jsonify(plane)
 
 
-# plane Schema
-class planechema(ma.Schema):
+
+# Staff Schema
+class StaffSchema(ma.Schema):
     class Meta:
         fields =('id', 'nameplane', 'genplane')
 
 # Init Schema 
-plane_schema = planechema()
-plane_schema = planechema(many=True)
-
-
-@app.route('/plane', methods=['GET'])
-def get_plane():
-    all_plane = plane.query.all()
-    result = plane_schema.dump(all_plane)
-    return jsonify(result)
-# ######################################################finish plane
-
-# #####################################################Begin 
-
-
-# #####################################################finish 
+staff_schema = StaffSchema()
+staffs_schema = StaffSchema(many=True)
 
 # Web Root Hello
 @app.route('/', methods=['GET'])
 def get():
     return jsonify({'ms': 'Hello Cloud Patcharapong'})
 
+@app.route('/planes', methods=['GET'])
+def get_staffs():
+    all_staffs = planes.query.all()
+    result = staffs_schema.dump(all_staffs)
+    return jsonify(result)
+
+
 # Run Server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run()
+    #app.run(host='0.0.0.0', port=80)
